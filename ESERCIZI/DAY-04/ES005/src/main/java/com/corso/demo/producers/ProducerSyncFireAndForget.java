0e926aa2e7325ef9c5414f505a2f81ca42a93bd5
aminelli <antonio.minelli@etlforma.com>
2026-03-05 09:12:57 +0100

    upd

--- ESERCIZI/DAY-04/ES005/src/main/java/com/corso/demo/producers/ProducerSyncFireAndForget.java
+++ ESERCIZI/DAY-04/ES005/src/main/java/com/corso/demo/producers/ProducerSyncFireAndForget.java
@@ -29,7 +29,7 @@ public class ProducerSyncFireAndForget {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        try (KafkaProducer<String, String> producer = new KafkaProducer<>(props)) {
+        try (final KafkaProducer<String, String> producer = new KafkaProducer<>(props)) {
             ProducerRecord<String, String> record = null;
             
             
